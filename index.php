98eb7acdd91a0ecdc44482d9877020fa6b2f3362
Perry <thescratcherman@gmail.com>
2015-04-12 16:10:58 -0500

    Added section on GET, POST, and SESSION

--- index.php
+++ index.php
@@ -137,6 +137,46 @@ for($number = 5; $number > 0; $number --) {
 
 
 /**
+ * GET, POST, and SESSION
+ *
+ * Remember, GET and POST are for getting data from the client.
+ * GET - visible in URL
+ * POST - not visible in URL
+ *
+ * SESSION is for keeping data persistet between pages, for example, the ID of
+ * a logged in user.
+ *
+ * TODO COOKIES
+ */
+
+// Retrieve data from a GET method
+// (ie method="get" in an HTML form from the previous page.)
+$username = $_GET['username'];
+
+// Same thing for a POST method but use $_POST instead of $_GET
+$color = $_POST['favorite_color'];
+
+// Session works the same way except that you have to call...
+session_start();
+// ...before you can do anything.
+
+// also, session variables you set on your own, whereas GET and POST are set
+// on the client side.
+$_SESSION['favorite_MLP_character'] = "PewDiePie";
+
+// The string between the []s (AKA the Key) can be whatever you want it to be,
+// and you can use that string whenever to retrieve or modify the value it
+// holds, even on a different page entirely.
+
+$pony = $_SESSION['favorite_MLP_character'];
+echo $pony; // prints 'PewDiePie'
+
+// Each session is unique to that user. If you want to delete all session
+// variables stored for this user, do this:
+session_unset();
+
+
+/**
  * WORKING WITH DATABASES
  */
 
